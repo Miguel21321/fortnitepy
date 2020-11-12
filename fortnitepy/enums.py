869b5a1593912bf3e8468465f4bafb2ddc79ed18
gomashio1596 <53356872+gomashio1596@users.noreply.github.com>
2020-11-12 23:45:50 +0900

    PlayStation 5

--- fortnitepy/enums.py
+++ fortnitepy/enums.py
@@ -163,14 +163,15 @@ class Region(Enum):
 
 
 class Platform(Enum):
-    WINDOWS     = 'WIN'
-    MAC         = 'MAC'
-    PLAYSTATION = 'PSN'
-    XBOX        = 'XBL'
-    SWITCH      = 'SWT'
-    IOS         = 'IOS'
-    ANDROID     = 'AND'
-    XBOXSERIESX = 'XBS'
+    WINDOWS      = 'WIN'
+    MAC          = 'MAC'
+    PLAYSTATION  = 'PSN'
+    XBOX         = 'XBL'
+    SWITCH       = 'SWT'
+    IOS          = 'IOS'
+    ANDROID      = 'AND'
+    XBOXSERIESX  = 'XBS'
+    PLAYSTATION5 = 'PS5'
 
 
 class UserSearchPlatform(Enum):
